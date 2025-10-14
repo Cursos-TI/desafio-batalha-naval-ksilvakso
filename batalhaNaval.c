6c9e8755be57a31742e2f6d6cad39ba2bb8f1431
ksilvakso <analisedesenvolvimentosistema2@gmail.com>
2025-10-14 21:51:13 +0000

    BATALHA NAVAL NIVEL NOVATO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,29 @@
 #include <stdio.h>
-
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
+ 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+    char *nome1[] = {"1 " "   0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome2[] = {"2 " "   0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome3[] = {"3 " "   0 " " 0 " " 0 " " 0 " " 8 " " 8 " " 8 " " 0 " " 0 " " 0 "};
+    char *nome4[] = {"4 " "   0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome5[] = {"5 " "   0 " " 0 " " 8 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome6[] = {"6 " "   0 " " 0 " " 8 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome7[] = {"7 " "   0 " " 0 " " 8 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome8[] = {"8 " "   0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome9[] = {"9 " "   0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nome10[] = {"10 " "  0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 " " 0 "};
+    char *nomes[] = {"BN " "  A " " B " " C " " D " " E " " F " " G " " H " " I " " J "};
+    int i;
+    for(i = 0; i < 10; i++) {
+        printf("%s\n", nomes[i]);
+        printf("%s\n", nome1[i]);
+        printf("%s\n", nome2[i]);
+        printf("%s\n", nome3[i]);
+        printf("%s\n", nome4[i]);
+        printf("%s\n", nome5[i]);
+        printf("%s\n", nome6[i]);
+        printf("%s\n", nome7[i]);
+        printf("%s\n", nome8[i]);
+        printf("%s\n", nome9[i]);
+        printf("%s\n", nome10[i]);
+    }
 }
